6d6ceba1c7a951939031e8cb1c0528097ed0d12c
AGarcesS <andreags_104@hotmail.com>
2022-10-20 17:20:09 +0200

    Añadido: generadas RAM específica y RAM general

--- RAM_DMA_PIC.srcs/sources_1/ram_general.vhd
+++ RAM_DMA_PIC.srcs/sources_1/ram_general.vhd
@@ -1,24 +1,20 @@
-
 LIBRARY IEEE;
 USE IEEE.std_logic_1164.all;
 USE IEEE.numeric_std.all;
 
 USE work.PIC_pkg.all;
 
-ENTITY ram_especifica IS
+ENTITY ram_general IS
 PORT (
    Clk      : in    std_logic;
    Reset    : in    std_logic;
    write_en : in    std_logic;
    oe       : in    std_logic;
    address  : in    std_logic_vector(3 downto 0);
-   databus  : inout std_logic_vector(7 downto 0);
-   switches : out   std_logic_vector(7 downto 0);
-   temp_l   : out   std_logic_vector(6 downto 0);
-   temp_h   : out   std_logic_vector(6 downto 0));   
-END ram_especifica;
+   databus  : inout std_logic_vector(7 downto 0)); 
+END ram_general;
 
-ARCHITECTURE behavior OF ram_especifica IS
+ARCHITECTURE behavior OF ram_general IS
 
   SIGNAL contents_ram : array8_ram(15 downto 0);
 
