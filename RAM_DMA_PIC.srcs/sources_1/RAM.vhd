f06ac235f5e172f9e97fc54a418d241b0b0e2c2d
davidherguetasoto <david.hergueta.soto@alumnos.upm.es>
2022-10-27 18:00:49 +0200

    Terminada RAM top

--- RAM_DMA_PIC.srcs/sources_1/RAM.vhd
+++ RAM_DMA_PIC.srcs/sources_1/RAM.vhd
@@ -21,6 +21,7 @@ END RAM;
 ARCHITECTURE behavior OF RAM IS
   
   --SIGNAL contents_ram : array8_ram(255 downto 0);
+  signal CS_ram_general, CS_ram_especifica:std_logic;
   component ram_especifica is 
     PORT (
         Clk      : in    std_logic;
@@ -28,6 +29,7 @@ ARCHITECTURE behavior OF RAM IS
         write_en : in    std_logic;
         oe       : in    std_logic;
         address  : in    std_logic_vector(3 downto 0);
+        CS       : in    std_logic;
         databus  : inout std_logic_vector(7 downto 0);
         switches : out   std_logic_vector(7 downto 0);
         temp_l   : out   std_logic_vector(6 downto 0);
@@ -45,14 +47,29 @@ ARCHITECTURE behavior OF RAM IS
   end component;
   
 BEGIN
-   RAM_ESPECIFICA:ram_especifica
+   INST_RAM_ESPECIFICA:ram_especifica
         port map(
             Clk => Clk,
             Reset => Reset,
             write_en => write_en,
             oe => oe, 
-            address => ,
-            CS =>  );
+            address => address,
+            CS =>  CS_ram_especifica,
+            databus => databus,
+            switches => switches,
+            temp_l => temp_l,
+            temp_h => temp_h);
             
+  INST_RAM_GENERAL:ram_general
+        port map(
+            Clk => Clk,
+            write_en => write_en,
+            oe => oe,
+            address => address,
+            CS => CS_ram_general,
+            databus => databus);
+            
+  CS_ram_especifica<= '1' when unsigned(address) < unsigned(X"40") else '0';
+  CS_ram_general<= '1' when unsigned(address) >= unsigned(X"40") else '0';
 END behavior;
 
